b52d864e1434657e3345b565e5605ece41395768
Akash Jag <akashjagannathan408@gmail.com>
2020-02-10 19:11:54 -0800

    backend template

---
+++ backend/Models/Student.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let studentSchema = new Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  collegeName: {
+    type: String
+  }
+}, {
+    collection: 'students'
+  })
+
+module.exports = mongoose.model('Student', studentSchema)
