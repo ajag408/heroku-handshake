26f6098ef95ed7b85a4da3e0bc3bb36554812305
Akash Jag <akashjagannathan408@gmail.com>
2020-02-11 19:47:34 -0800

    working on front-end layout

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -3,10 +3,16 @@ import "bootstrap/dist/css/bootstrap.css";
 export default class Home extends Component {
 
   render() {
-    return (<div>
+    return (<body class = "text-center">
+            <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header class = "masthead mb-auto">
+                <div class = "inner">
+                    <h3 class = "masthead-brand">Handshake</h3>
+                </div>
+            </header>
+        <a href ="/signup">Sign up</a>
 
-    <a href ="/signup">Sign up</a>
-
-    </div>);
+        </div>
+    </body>);
   }
 }
