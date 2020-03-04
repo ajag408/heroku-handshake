ccbd116cb84a7190f07ba8fb7462988aa3c3f496
Akash Jag <akashjagannathan408@gmail.com>
2020-03-04 12:30:05 -0800

    installed eslint on front-end

---
+++ Dump (1)/reactHandshake_registered_student_event0.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.6.33, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: reactHandshake
+-- ------------------------------------------------------
+-- Server version	5.6.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `registered_student_event`
+--
+
+DROP TABLE IF EXISTS `registered_student_event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registered_student_event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event` int(11) NOT NULL,
+  `student` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_index` (`event`,`student`),
+  KEY `student` (`student`),
+  CONSTRAINT `registered_student_event_ibfk_1` FOREIGN KEY (`student`) REFERENCES `students` (`id`),
+  CONSTRAINT `registered_student_event_ibfk_2` FOREIGN KEY (`event`) REFERENCES `events` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registered_student_event`
+--
+
+LOCK TABLES `registered_student_event` WRITE;
+/*!40000 ALTER TABLE `registered_student_event` DISABLE KEYS */;
+INSERT INTO `registered_student_event` VALUES (5,2,1),(8,2,2),(3,4,1);
+/*!40000 ALTER TABLE `registered_student_event` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-27 20:32:21
