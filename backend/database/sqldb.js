05c24a1a059273976421b1a72c25d2b88d5a7476
Akash Jag <akashjagannathan408@gmail.com>
2020-03-06 19:53:26 -0800

    heroku

--- backend/database/sqldb.js
+++ backend/database/sqldb.js
@@ -3,11 +3,11 @@ const mysql = require('mysql');
 
 // local mysql db connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '8889',
-  user: 'root',
-  password: 'root',
-  database: 'reactHandshake',
+  host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: '3306',
+  user: 'pvgt0ikkvj7xf93e',
+  password: 'lf71yxojh35mybjz',
+  database: 'rqxyzqga475wwky1',
 });
 
 connection.connect((err) => {
